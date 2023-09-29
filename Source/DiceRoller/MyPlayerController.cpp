89f865fa2d4d55c963d9d30fd976db7f4d539f92
NancyMakesThings <nancywei96@gmail.com>
2023-09-28 23:58:00 -0700

    Converted existing enhanced input action and BPs to C++

--- Source/DiceRoller/MyPlayerController.cpp
+++ Source/DiceRoller/MyPlayerController.cpp
@@ -2,6 +2,11 @@
 
 
 #include "MyPlayerController.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Dice.h"
+#include "Kismet/KismetMathLibrary.h"
 
 AMyPlayerController::AMyPlayerController()
 {
@@ -14,7 +19,11 @@ AMyPlayerController::AMyPlayerController()
 void AMyPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
-
+	
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(DiceRollerMappingContext,0);
+	}
 }
 
 // Called every frame
@@ -22,7 +31,8 @@ void AMyPlayerController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (IsGrabbingComponent)
+	// Update grabbed dice position
+	if (PhysicsHandle->GrabbedComponent != NULL)
 	{
 		FVector MouseLocation, MouseDirection, NewDiceLocation;
 		DeprojectMousePositionToWorld(MouseLocation, MouseDirection);
@@ -35,3 +45,39 @@ void AMyPlayerController::Tick(float DeltaTime)
 
 }
 
+void AMyPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent)) {
+		EnhancedInputComponent->BindAction(DiceMoveAction, ETriggerEvent::Started, this, &AMyPlayerController::DiceDrag);
+		EnhancedInputComponent->BindAction(DiceMoveAction, ETriggerEvent::Completed, this, &AMyPlayerController::DiceDrop);
+	}	
+}
+
+void AMyPlayerController::DiceDrag()
+{
+	UE_LOG(LogTemp, Warning, TEXT("IA_DiceMove started"));
+
+	FHitResult HitResult;
+	GetHitResultUnderCursorByChannel(UEngineTypes::ConvertToTraceType(ECC_Visibility), true, HitResult);
+	ADice* Dice = Cast<ADice>(HitResult.GetActor());
+	if (Dice != nullptr)
+	{
+		OriginalDistanceToObject = HitResult.Distance;
+		UPrimitiveComponent* HitComponent = HitResult.GetComponent();
+		PhysicsHandle->GrabComponentAtLocationWithRotation(HitComponent, NAME_None, HitComponent->GetCenterOfMass(), HitComponent->GetOwner()->GetActorRotation());
+	}
+}
+
+void AMyPlayerController::DiceDrop()
+{
+	UE_LOG(LogTemp, Warning, TEXT("IA_DiceMove completed"));
+	
+	if (PhysicsHandle->GrabbedComponent != NULL)
+	{
+		FVector Impulse = FVector(UKismetMathLibrary::RandomFloatInRange(-1, 1), UKismetMathLibrary::RandomFloatInRange(-1, 1), UKismetMathLibrary::RandomFloatInRange(-1, 1));
+		Impulse = Impulse * AngularImpulseMagnitude;
+		PhysicsHandle->GrabbedComponent->AddAngularImpulseInDegrees(Impulse, NAME_None, true);
+		PhysicsHandle->ReleaseComponent();
+	}
+}
--- Source/DiceRoller/MyPlayerController.h
+++ Source/DiceRoller/MyPlayerController.h
@@ -5,7 +5,11 @@
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "InputActionValue.h"
 #include "MyPlayerController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
 
 /**
  * 
@@ -17,24 +21,35 @@ class DICEROLLER_API AMyPlayerController : public APlayerController
 public:
 	AMyPlayerController();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-
-
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Input
+	void SetupInputComponent() override;
+	void DiceDrag();
+	void DiceDrop();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputMappingContext* DiceRollerMappingContext;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* DiceMoveAction;
+
+
+	// Update dice location
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	float GrabLocationZ = 800;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float OriginalDistanceToObject;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	bool IsGrabbingComponent = false;
-	UPROPERTY(EditDefaultsOnly, Category = "Components", BlueprintReadWrite)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Components")
 	UPhysicsHandleComponent* PhysicsHandle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	float AngularImpulseMagnitude = 360;
+
 
 };
